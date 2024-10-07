9cc44e81a5a72a7a6e539fdd4e6caa340e0d0d93
Alex <hbostic@gmail.com>
2024-10-07 09:27:47 -0400

    Working on elevator implementation

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/elevator/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.mechanisms.elevator.subsystems;
+
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class ElevatorSubsystem {
+
+    private MotorEx ER;
+    private MotorEx EL;
+
+    private Telemetry telemetry;
+    private DcMotorEx.Direction direction;
+    private double power = 0.0;
+    //6. Added in the ability to throttle power
+    private double powerRatio = 0.75;
+    //7. Created a runmode variable (https://docs.ftclib.org/ftclib/v/v2.0.0/features/hardware/motors#using-a-runmode)
+    private DcMotorEx.RunMode mode;
+
+    private static final int MIN_TARGET_POSITION = 0;
+    private int maxTargetPosition = 4250;
+
+    public ElevatorSubsystem(MotorEx ERMotor, MotorEx ELMotor  ){
+        ER = ERMotor;
+        EL = ELMotor;
+    }
+    //9. Added another constructor that takes in Telemetry
+    public ElevatorSubsystem(MotorEx ERMotor, MotorEx ELMotor, Telemetry telemetry){
+
+        ER = ERMotor;
+        EL = ELMotor;
+        this.telemetry = telemetry;
+    }
+    public void stopResetEncoder(){
+        //setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+    }
+
+    public void setZeroPowerBehavoir(DcMotorEx.ZeroPowerBehavior behavior){
+        ER.motorEx.setZeroPowerBehavior(behavior);
+        EL.motorEx.setZeroPowerBehavior(behavior);
+    }
+
+    public void setDirection(DcMotorEx.Direction direction){
+        this.direction = direction;
+        ER.motor.setDirection(this.direction);
+        EL.motor.setDirection(this.direction);
+    }
+
+    public void setPower(double power){
+        this.power = power;
+        ER.set(this.power);
+        EL.set(this.power);
+    }
+
+    public void setMode(DcMotorEx.RunMode mode){
+        this.mode = mode;
+        ER.motorEx.setMode(this.mode);
+        EL.motorEx.setMode(this.mode);
+    }
+
+    public void setBrake(){
+        setZeroPowerBehavoir(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void stopElevator(){
+        ER.stopMotor();
+        EL.stopMotor();
+    }
+
+    public void setInverted(boolean inv){
+        ER.setInverted(inv);
+        EL.setInverted(inv);
+    }
+
+
+    public double getERPower(){
+        return ER.motorEx.getPower();
+    }
+    public double getELPower(){
+        return EL.motorEx.getPower();
+    }
+
+    public DcMotorSimple.Direction getERDirection(){
+        return ER.motorEx.getDirection();
+    }
+
+    public DcMotorSimple.Direction getDirection(){
+        return ER.motorEx.getDirection();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ElevatorTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ElevatorTeleOp.java
@@ -8,13 +8,11 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 
 @Config
-@TeleOp(name="MecanumTeleOp", group="FreightFrenzy")
-public class MecanumTeleOp extends LinearOpMode {
+@TeleOp(name="ElevatorTeleOp", group="InToTheDeep")
+public class ElevatorTeleOp extends LinearOpMode {
 
 
     @Override
@@ -23,9 +21,9 @@ public class MecanumTeleOp extends LinearOpMode {
 
 
         //import the hardware map
-        Motor fl = new Motor(hardwareMap, "FL");
+        Motor el = new Motor(hardwareMap, "EL");
         Motor bl = new Motor(hardwareMap, "BL");
-        Motor fr = new Motor(hardwareMap, "FR");
+        Motor er = new Motor(hardwareMap, "ER");
         Motor br = new Motor(hardwareMap, "BR");
 
 
@@ -85,17 +83,17 @@ public class MecanumTeleOp extends LinearOpMode {
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = ((y + x + rx) / denominator);
-            double backLeftPower = ((y - x + rx) / denominator);
-            double frontRightPower = ((y - x - rx) / denominator);
-            double backRightPower = ((y + x - rx) / denominator);
+            double leftPower = ((y + x + rx) / denominator);
+            //double backLeftPower = ((y - x + rx) / denominator);
+            double rightPower = ((y - x - rx) / denominator);
+            //double backRightPower = ((y + x - rx) / denominator);
 
 
 
-            fl.motor.setPower(frontLeftPower);
-            bl.motor.setPower(backLeftPower);
-            fr.motor.setPower(frontRightPower);
-            br.motor.setPower(backRightPower);
+            el.motor.setPower(leftPower);
+            //bl.motor.setPower(backLeftPower);
+            er.motor.setPower(rightPower);
+            //br.motor.setPower(backRightPower);
 
 
             telemetry.update();
