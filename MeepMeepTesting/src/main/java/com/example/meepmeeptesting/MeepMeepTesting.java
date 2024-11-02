88df318c7b3b8c2d4b0436934ef0eaf964da085a
DESKTOP-AUURQ7F\BC4HSTEM <cms0679@outlook.com>
2024-11-02 14:23:04 -0400

    Izeyah- Started the red allaince side automous start in meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -12,16 +13,39 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
+                .setConstraints(60, 60, Math.toRadians(90), Math.toRadians(90), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-38, -55, 0))
                         .turn(Math.toRadians(90))
-                        .forward(30)
+                        .forward(5)
                         .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                        .forward(10)
+                        .turn(Math.toRadians(35))
+                        .forward(5)
+                        //put the thing down
+                        .back(5)
+                        .turn(Math.toRadians(-27))
+                        .back(10)
+                        .turn(Math.toRadians(-90))
+                        //base code to start the different paths maybe ^
+                        .forward(18.5)
+                        .turn(Math.toRadians(30))
+                        .forward(1.5)
+                        /*.turn(Math.toRadians(-90))
+                        .forward(50)
+                        .splineTo(new Vector2d(45, 50), Math.toRadians(0))
+                        .turn(Math.toRadians(-90))
+                        .forward(80)
+                        .splineTo(new Vector2d(-40, -26), Math.toRadians(90))
+                        .turn(Math.toRadians(-90))
+                        .forward(90)
+                        .turn(Math.toRadians(-90))
+                        .forward(60)
+                        .forward(60)
+                        .strafeLeft(60)
+                        .back(60)
+                        .strafeRight(60)
+                        .forward(60)
+                        .strafeLeft(60)*/
                         .build());
 
 
