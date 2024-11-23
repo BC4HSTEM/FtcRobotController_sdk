11e6f0926f8f9ae6861cd1000dae159110e1b78d
DESKTOP-AUURQ7F\BC4HSTEM <cms0679@outlook.com>
2024-11-22 23:47:48 -0500

    Arm Teleop Setting confirmed for the most part

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ArmTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/ArmTeleop.java
@@ -13,10 +13,12 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 public class ArmTeleop extends OpMode {
 
     private PIDController pidController;
-    public static double p = 0, i = 0, d = 0;
-    public static double f = 0;
+    public static double p = .06, i = 0, d = 0.0001;
+    public static double f = 0.08;
 
-    public int target = 0;
+
+    //downPos = 102;
+    public static int target = 50;
 
     private final double ticks_in_degree = 288.0 / 360.0;
 
@@ -45,14 +47,15 @@ public class ArmTeleop extends OpMode {
 
         int armPos = arm_motor.getCurrentPosition();
         double pid = pidController.calculate(armPos, target);
-        double ff = Math.cos(Math.toRadians(target / ticks_in_degree));
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
-        double power = pid * ff;
+        double power = pid + ff;
 
         arm_motor.setPower(power);
 
         telemetry.addData("pos", armPos);
         telemetry.addData("target" , target);
+        telemetry.addData("power", power);
         telemetry.update();
 
 
