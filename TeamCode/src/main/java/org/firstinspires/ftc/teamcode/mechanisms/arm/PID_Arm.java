dfcfac79e310aeb2f4c0ca844d424d418f46d849
DESKTOP-AUURQ7F\BC4HSTEM <cms0679@outlook.com>
2025-01-09 20:57:51 -0500

    Izeyah PID ARM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/arm/PID_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/arm/PID_Arm.java
@@ -1,2 +1,27 @@
-package org.firstinspires.ftc.teamcode.mechanisms.arm;public class PID_Arm {
+package org.firstinspires.ftc.teamcode.mechanisms.arm;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class PID_Arm extends OpMode {
+    private PIDController controller;
+
+    public static double p = 0, i =0, d = 0;
+    public static double f = 0;
+
+    public static int target = 0;
+
+    private final double ticks_in_degree = 700 / 100.0;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
